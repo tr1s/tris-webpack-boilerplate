9bdf1aa0523d7cb6ef34627f6da48386ba9eac6a
Tristan Michael Lawrence <trislaw@gmail.com>
2018-12-16 22:43:17 -0500

    v1 💎⛏

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-require('offline-plugin/runtime').install();
+const runtime = require('offline-plugin/runtime');
+
+runtime.install({
+  onUpdating: () => {
+    console.log('SW Event:', 'onUpdating');
+  },
+  onUpdateReady: () => {
+    console.log('SW Event:', 'onUpdateReady');
+    // Update is applied here
+    runtime.applyUpdate();
+  },
+  onUpdated: () => {
+    console.log('SW Event:', 'onUpdated');
+    // Force reload happens here
+    window.location.reload();
+  },
+
+  onUpdateFailed: () => {
+    console.log('SW Event:', 'onUpdateFailed');
+  }
+});
 
 import './fonts/LibreBaskerville-Regular.ttf';
 
