cfda829a422e8f57b33afb7e23167da2e4e42c91
Tristan Michael Lawrence <trislaw@gmail.com>
2018-12-17 00:38:08 -0500

    💎⛏

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-const runtime = require('offline-plugin/runtime');
-
-// runtime.install({
-//   onUpdating: () => {
-//     console.log('SW Event:', 'onUpdating');
-//   },
-//   onUpdateReady: () => {
-//     console.log('SW Event:', 'onUpdateReady');
-//     // Tells to new SW to take control immediately
-//     runtime.applyUpdate();
-//   },
-//   onUpdated: () => {
-//     console.log('SW Event:', 'onUpdated');
-//     // Reload the webpage to load into the new version
-//     window.location.reload();
-//   },
-
-//   onUpdateFailed: () => {
-//     console.log('SW Event:', 'onUpdateFailed');
-//   }
-// });
+require('offline-plugin/runtime').install();
 
 import './fonts/LibreBaskerville-Regular.ttf';
 
