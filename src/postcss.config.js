3dac98af74fa139e0439cbafd65ec01155c394b8
Tristan Michael Lawrence <trislaw@gmail.com>
2018-12-23 16:07:05 -0500

    last two browsers for autoprefixing

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
+    require('autoprefixer')({
+      browsers: 'last 2 versions',
+    }),
     require('cssnano')({
         preset: 'default',
     })
