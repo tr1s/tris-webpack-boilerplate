21335db184e968e2f7aa573aa37f77797b737268
Tristan Michael Lawrence <trislaw@gmail.com>
2019-01-04 13:07:45 -0500

    added babelrl, fixed targets

--- webpack.common.js
+++ webpack.common.js
@@ -72,10 +72,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
+          loader: 'babel-loader'
         }
       }
     ]
