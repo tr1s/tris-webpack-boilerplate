1fda91b2b0371568a0041cd9994d4100e147ff0c
Tristan Michael Lawrence <trislaw@gmail.com>
2019-01-09 12:09:01 -0500

    added purgecss

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,3 +1,5 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
   plugins: [
     require('autoprefixer')({
@@ -5,6 +7,10 @@ module.exports = {
     }),
     require('cssnano')({
         preset: 'default',
+    }),
+    purgecss({
+      content: ['./**/*.html'],
+      keyframes: true
     })
   ]
 }
