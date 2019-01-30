acfd62b242c8326b63a73b158fdbb24fce5d964b
Tristan Michael Lawrence <trislaw@gmail.com>
2019-01-29 19:43:35 -0500

    reverted webpack merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,7 @@
-const { join } = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const { WebpackPluginServe } = require('webpack-plugin-serve');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  entry: ['webpack-plugin-serve/client'],
-  plugins: [
-    new WebpackPluginServe({
-      host: 'localhost',
-      open: true,
-      static: join(__dirname, 'dist'),
-    })
-  ],
-  watch: true,
 });
