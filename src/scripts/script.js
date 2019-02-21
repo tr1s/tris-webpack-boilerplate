54cfec7d2f135026944c8c2cff6afabfc30eb065
Tristan Michael Lawrence <trislaw@gmail.com>
2019-02-21 10:58:40 -0500

    🖊 fixed console.log typo

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,4 @@
 console.log('%c Hello from /scripts/script.js ', 'background: lemonchiffon; border: 1px solid #fff');
-console.log('%c Welcome to trs-webpack-boilerplate! ', 'background: lavenderblush; border: 1px solid #fff');
+console.log('%c Welcome to tris-webpack-boilerplate! ', 'background: lavenderblush; border: 1px solid #fff');
 
 // add custom js below
