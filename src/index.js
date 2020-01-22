d3b61abc59e5e4d260f9a36d20f312d85238a255
Tristan Lawrence <trislaw@gmail.com>
2020-01-22 01:09:34 -0500

    🌱 package updates, eslint, organizations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-require('offline-plugin/runtime').install();
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
 import './fonts/libre-baskerville-v5-latin-regular.woff';
 import './fonts/libre-baskerville-v5-latin-regular.woff2';
 
 import './index.html';
 import './index.scss';
-import './scripts/script.js';
+import './scripts/script';
+
+OfflinePluginRuntime.install();
