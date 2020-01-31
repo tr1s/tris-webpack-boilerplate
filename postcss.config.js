26977580f2a563abed7672737053bf1372caea17
Tristan Lawrence <trislaw@gmail.com>
2020-01-30 22:48:50 -0500

    lint

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,22 @@
 const purgecss = require('@fullhuman/postcss-purgecss');
 const cssnano = require('cssnano');
 
-module.exports = (ctx) => {
+module.exports = ctx => {
   return {
     plugins: [
       require('autoprefixer'),
-      ...ctx.options.env === 'production' ? [
-        cssnano({
-          preset: 'default',
-        }),
-        purgecss({
-          content: ['./**/*.html'],
-          keyframes: true,
-          defaultExtractor: content => content.match(/[A-z0-9-:/]+/g)
-        })
-      ] : []
+      ...(ctx.options.env === 'production'
+        ? [
+            cssnano({
+              preset: 'default'
+            }),
+            purgecss({
+              content: ['./**/*.html'],
+              keyframes: true,
+              defaultExtractor: content => content.match(/[A-z0-9-:/]+/g)
+            })
+          ]
+        : [])
     ]
-  }
-}
+  };
+};
