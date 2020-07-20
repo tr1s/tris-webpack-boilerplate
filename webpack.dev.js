a6fc9d088980a408decff4e5fade4f20208bc3a6
Tristan Lawrence <trislaw@gmail.com>
2020-07-19 23:25:51 -0400

    🛠 destructured merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
