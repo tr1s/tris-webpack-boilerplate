de5b54d85f5bd6402a8676ca4501393ce6b2b399
Tristan Lawrence <trislaw@gmail.com>
2021-02-07 17:39:53 -0500

    added compression plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,29 +2,17 @@ const paths = require("./paths");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
+const CompressionPlugin = require("compression-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: false,
-  output: {
-    path: paths.build,
-    publicPath: "/",
-    filename: "js/[name].[contenthash].bundle.js",
-  },
-  plugins: [
-    // Extracts CSS into separate files
-    // Note: style-loader is for development, MiniCssExtractPlugin is for production
-    new MiniCssExtractPlugin({
-      filename: "styles/[name].[contenthash].css",
-      chunkFilename: "[id].css",
-    }),
-  ],
   module: {
     rules: [
       {
-        test: /\.(scss|css)$/,
+        test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
@@ -40,9 +28,20 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    // Extracts CSS into separate files
+    // Note: style-loader is for development, MiniCssExtractPlugin is for production
+    new MiniCssExtractPlugin({
+      filename: "styles/[name].[contenthash].css",
+      chunkFilename: "[id].css",
+    }),
+    new CompressionPlugin({
+      test: /\.(html|css|js)(\?.*)?$/i,
+    }),
+  ],
   optimization: {
     minimize: false,
-    minimizer: [new CssMinimizerPlugin(), "..."],
+    minimizer: [new CssMinimizerPlugin()],
     // Once your build outputs multiple chunks, this option will ensure they share the webpack runtime
     // instead of having their own. This also helps with long-term caching, since the chunks will only
     // change when actual code changes, not the webpack runtime.
@@ -54,5 +53,10 @@ module.exports = merge(common, {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000,
+  },
+  output: {
+    path: paths.build,
+    publicPath: "/",
+    filename: "js/[name].[contenthash].bundle.js",
   },
 });
